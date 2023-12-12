8ef8f449d49596b468b83ba849ae8f0d13a44252
haris18896 <haris18896@gmail.com>
2023-12-12 16:56:38 +0500

    working with android version

--- src/Styles/Pages/index.js
+++ src/Styles/Pages/index.js
@@ -1,7 +1,1 @@
 export * from './auth';
-export * from './home';
-export * from './Splash';
-export * from './VQEligibilty';
-export * from './speedometer';
-export * from './ShotStatistics';
-export * from './CumulativeStatistics';
